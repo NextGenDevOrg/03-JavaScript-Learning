eafadfdaa6de2ba2885049f5440695d7a3deb918
Mezab khan <90905125+coder-mezab@users.noreply.github.com>
2021-10-30 14:54:52 +0530

    Update tasks-by-users/coder-mezab/05-search-movies-by-filter/assets/scripts/objects.js
    Co-authored-by: Alisha <39584721+AlishaPrasad@users.noreply.github.com>

--- tasks-by-users/coder-mezab/05-search-movies-by-filter/assets/scripts/objects.js
+++ tasks-by-users/coder-mezab/05-search-movies-by-filter/assets/scripts/objects.js
@@ -26,7 +26,7 @@ const renderMovies = (filter = "") => {
     let text = movieTitle + " - ";
     for (let key in info) {
       if (key !== "title") {
-        text = text + `${key}: ${info[key]} `;
+        text = `${text}${key}: ${info[key]} `;
       }
     }
     movieEl.textContent = text;
