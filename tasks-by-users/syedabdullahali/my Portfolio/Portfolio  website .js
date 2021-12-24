31d20d6f1f5a125186d00775447389fd67240d6d
syed abdullah ali <87133797+syedabdullahali@users.noreply.github.com>
2021-12-24 14:13:34 +0530

    Update Portfolio  website .js

--- tasks-by-users/syedabdullahali/my Portfolio/Portfolio  website .js
+++ tasks-by-users/syedabdullahali/my Portfolio/Portfolio  website .js
@@ -207,10 +207,3 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
-
-
-
-
-
-
-
